63ae667926db4f3a0e2cf223e169fb366b67c0ca
후니 <hipark@huniui-MacBookPro.local>
2024-10-31 05:19:14 +0900

    chatgpt fine tuning complete

--- app/router/router.py
+++ app/router/router.py
@@ -3,8 +3,9 @@ import os
 
 from fastapi import APIRouter, Request, Response
 
-from ..service.data import get_data_from_storage, get_latest_version, upload_folder 
-from ..service.model import Trainer
+from ..service.data import get_data_from_storage, get_latest_version, upload_folder, download_from_s3
+from ..service.chatgpt_tuning import make_tuning_dataset, start_training
+# from ..service.model import Trainer
 
 router = APIRouter(
     prefix='/train',
@@ -15,13 +16,30 @@ metadata = {
     'description': 'Model Train Service'
 }
 
-@router.get("/start")
+# chatgpt fine-tuning
+@router.get('/start', tags=['Train'])
 async def train(model_id: str):
-    latest_version = await get_latest_version(model_id=model_id)
-    print(f'[INFO] filename: {latest_version}')
+    print('[INFO] ROUTER train')
+    latest_version = await get_latest_version(model_id)
+    downloaded_path = await download_from_s3(latest_version)
+    print(f'[INFO] ROUTER train - latest_version: {latest_version}')
+
+    dataset_id = await make_tuning_dataset(downloaded_path)
+    print(f'[INFO] ROUTER train - dataset_id: {dataset_id}')
+
+    gpt_id = await start_training(dataset_id)
+    print(f'[INFO] ROUTER train - gpt_id: {gpt_id}')
+
+    return gpt_id
+
+# # QLoRa
+# @router.get("/start")
+# async def train(model_id: str):
+#     latest_version = await get_latest_version(model_id=model_id)
+#     print(f'[INFO] filename: {latest_version}')
     
-    get_data_from_storage(object_key=latest_version)
+#     get_data_from_storage(object_key=latest_version)
 
-    saved_model_path = Trainer(object_key=latest_version)
+#     saved_model_path = Trainer(object_key=latest_version)
 
-    upload_folder(saved_model_path)
+#     upload_folder(saved_model_path)
---
+++ app/service/chatgpt_tuning.py
@@ -0,0 +1,45 @@
+import os
+from openai import OpenAI
+import time
+
+from dotenv import load_dotenv
+load_dotenv()
+
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+client = OpenAI(api_key=openai_api_key)
+
+async def make_tuning_dataset(downloaded_path: str) -> str:
+    uploaded_dataset = client.files.create(
+        file=open(downloaded_path, 'rb'),
+        purpose='fine-tune'
+    )
+
+    dataset_id = uploaded_dataset.id
+
+    return dataset_id
+
+async def start_training(dataset_id: str):
+    print(f'[INFO] EXECUTE start_training()')
+
+    training_detail = client.fine_tuning.jobs.create(
+        training_file=dataset_id,
+        model='gpt-4o-mini-2024-07-18'
+    )
+
+    training_id = training_detail.id
+    print(f'[INFO] EXECUTE start_training() - training_id: {training_id}')
+    time.sleep(5)
+
+    while True:
+        message = client.fine_tuning.jobs.list_events(fine_tuning_job_id=training_id, limit=1).data[0].message
+        print(f'[INFO] EXECUTE start_training() - message: {message}')
+
+        if message == 'The job has successfully completed': break
+        else: time.sleep(60)
+
+    for job in client.fine_tuning.jobs.list():
+        if job.id == training_detail.id:
+            gpt_id = job.fine_tuned_model
+
+    return gpt_id
